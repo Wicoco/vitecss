48068e64edd79e4c1c1d3b7643bafd3e5656693f
victorfernel <victorfernel444@gmail.com>
2024-10-24 08:40:54 +0200

    maj

--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "./index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
